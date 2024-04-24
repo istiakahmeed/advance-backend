e1f46ea98423920947194d352b650dd8b1f9e572
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-04-24 20:20:54 +0600

    learn some about middleware

--- node-js/practice-project/server.js
+++ node-js/practice-project/server.js
@@ -3,62 +3,27 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const books = [
-  {
-    id: "1",
-    name: "Personal Finance",
-    price: 500,
-  },
-  {
-    id: "2",
-    name: "Javascript for dummies",
-    price: 1000,
-  },
-  {
-    id: "3",
-    name: "JavaScript the definitive guide",
-    price: 1500,
-  },
-  {
-    id: "4",
-    name: "You don't know js yet",
-    price: 2500,
-  },
-  {
-    id: "5",
-    name: "Atomic Habits",
-    price: 100,
-  },
-  {
-    id: "6",
-    name: "JavaScript the good parts",
-    price: 1200,
-  },
-];
+app.use(express.static(__dirname + "/public")); //server static middleware
+const simpleLogger = (req, res, next) => {
+  console.log(`Method: ${req.method}, URL: ${req.url}`);
+  next();
+};
 
+const secondMiddleware = (req, res, next) => {
+  console.log("Second Middleware");
+  next();
+};
+
+// app.use(simpleLogger); // global middleware
+
+app.use([simpleLogger, secondMiddleware]);
 app.get("/", (req, res) => {
-  if (req.query.show === "all") {
-    res.json(books);
-  }
-
-  if (req.query.price === "500") {
-    const result = books.filter((book) => book.price <= 500);
-    return res.json(result);
-  }
-
-  if (req.query.price === "1000") {
-    const result = books.filter((book) => book.price <= 1000);
-    return res.json(result);
-  }
-  return res.json(books);
-});
-app.post("/books", (req, res) => {
-  const book = req.body;
-  books.push(book);
-
-  res.json(books);
+  res.json({ message: "Sweet Home" });
 });
 
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello World" });
+});
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
